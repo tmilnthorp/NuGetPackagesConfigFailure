fb190f318796a443ca53faeceea3b561ccfaae15
Tristan Milnthorp <tristan.milnthorp@ansys.com>
2020-11-16 19:52:15 -0500

    Create managed project

---
+++ NuGetPackagesConfigFailure/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using UnitsNet;
+
+namespace NuGetPackagesConfigFailure
+{
+	class Program
+	{
+		static void Main( string[] args )
+		{
+			Console.WriteLine( Temperature.FromDegreesCelsius( 0.0 ) );
+		}
+	}
+}
